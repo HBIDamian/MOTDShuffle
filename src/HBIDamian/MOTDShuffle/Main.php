d048f02267afe470e4772933681029d3170e54ca
HBIDamian <hbidamian@gmail.com>
2022-01-23 18:18:26 +0000

    I did not hit her, it's not true! It's bullshit! I did not hit her! I did not! Oh Hi Mark!
    I did not hit her, it's not true! It's bullshit! I did not hit her! I did not! Oh Hi Mark!

--- src/HBIDamian/MOTDShuffle/Main.php
+++ src/HBIDamian/MOTDShuffle/Main.php
@@ -11,11 +11,10 @@ class Main extends PluginBase implements Listener {
         @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");        
         $this->getMainConfig = new Config($this->getdatafolder() . "config.yml", Config::YAML);
-        $this->getLogger()->info("has been activated!");
         if (is_int($this->getMainConfig()->get("MOTD Delay")) == true){
             $this->getScheduler()->scheduleRepeatingTask(new SendMOTD($this), $this->getMainConfig()->get("MOTD Delay"));
         } else {
-            $this->getLogger()->info("§4The value you entered in §c'MOTD Delay' §4is not an integer. Please fix it.");
+            $this->getLogger()->warning("The value you entered in 'MOTD Delay' is not an integer. Please fix it.");
         } 
     }
 
